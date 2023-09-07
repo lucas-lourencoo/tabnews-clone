f15b5d0c96d18a5cd2ba211b0dcf4bb3048b6b2a
Lucas Lourenço <lucascelestiano@gmail.com>
2023-09-07 02:17:48 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-clone
+
 Este é uma cópia do projeto https://tabnews.com.br feita na aula do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-export default function Home(){
-    return <h1>Oi amor, passando pra dizer que eu amo você!</h1>
+export default function Home() {
+  return <h1>Oi amor, passando pra dizer que eu amo você!</h1>;
 }
