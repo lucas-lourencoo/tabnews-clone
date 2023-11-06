a080837a793d0b7ea4f856953738dab52d4a2e9f
Lucas Lourenço <lucascelestiano@gmail.com>
2023-11-06 20:04:01 +0000

    add `database` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function status(req, res) {
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows[0].sum);
+
   return res.json("Olá!");
 }
