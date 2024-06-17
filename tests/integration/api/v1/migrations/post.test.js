4dfad91c8d8dd053b599f3abececb454f2a9c3ef
Lucas Lourenço <lucascelestiano@gmail.com>
2024-06-17 15:13:36 -0400

    make npm test more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-}
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -25,5 +25,5 @@ test("POST to /api/v1/migrations should return 200", async () => {
   const response2Body = await response2.json();
 
   expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0) ;
+  expect(response2Body.length).toBe(0);
 });
